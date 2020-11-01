07b364052e8c7acc6ccdd175c3e2c2c29356b960
kevinyohe <kevinyohe7@yahoo.com>
2020-11-01 17:16:05 -0500

    fix typos

--- ssg/content.py
+++ ssg/content.py
@@ -1,6 +1,7 @@
 import re
-from yaml import load, FullLoader
+
 from collections.abc import Mapping
+from yaml import load, FullLoader
 
 
 class Content(Mapping):
@@ -9,6 +10,38 @@ class Content(Mapping):
 
     @classmethod
     def load(cls, string):
-        _, fm, context = cls.__regex.spligit(string, 2)
-        load(fm, Loader=FullLoader)
+        _, fm, content = cls.__regex.split(string, 2)
+        metadata = load(fm, Loader=FullLoader)
         return cls(metadata, content)
+
+    def __init__(self, metadata, content):
+        self.data = metadata
+        self.data["content"] = content
+
+    @property
+    def body(self):
+        return self.data["content"]
+
+    @property
+    def type(self):
+        return self.data["type"] if "type" in self.data else None
+
+    @type.setter
+    def type(self, type):
+        self.data["type"] = type
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __iter__(self):
+        self.data.__iter__()
+
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        data = {}
+        for key, value in self.data.items():
+            if key != "content":
+                data[key] = value
+        return str(data)
